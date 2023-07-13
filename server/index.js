c6eadeb6cae70def22419f696005f33c493cf6cb
Bohdan Barulin <webshogun@proton.me>
2023-07-13 12:23:33 +0300

    Connected to the MongoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, () => {
-  console.log('Server starting...')
-})
+async function start() {
+	try {
+		await mongoose.connect(
+			`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.38bz2pv.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+		);
+		app.listen(process.env.PORT, () => console.log('Server started'));
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+start();
