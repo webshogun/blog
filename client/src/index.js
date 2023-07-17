3aa35004f9f4a1f036c96eacf44c25291d49dbcd
webshogun <webshogun@proton.me>
2023-07-17 12:28:47 +0300

    Connected the Redux toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>,
 );
