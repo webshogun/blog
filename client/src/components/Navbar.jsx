7f57205173f480b3b4872a02b6383b24a28a418b
webshogun <webshogun@proton.me>
2023-07-17 11:57:42 +0300

    Layout of login and registration pages

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
-	const isAuth = true;
+	const isAuth = false;
 	return (
 		<div className='flex py-4 justify-between items-center'>
 			<span className='flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm'>E</span>
