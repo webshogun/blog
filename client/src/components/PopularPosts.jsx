a931e42223f6831fb88ed5f6014aeb2ad8f67435
webshogun <webshogun@proton.me>
2023-07-25 21:18:33 +0300

    Edit post on backend

--- client/src/components/PopularPosts.jsx
+++ client/src/components/PopularPosts.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const PopularPosts = ({ post }) => {
   return (
     <div className="bg-gray-600 my-1">
-      <div className="flex text-xs p-2 text-gray-300 hover:bg-gray-800 hover:text-white">{post.title}</div>
+      <Link to={`${post._id}`} className="flex text-xs p-2 text-gray-300 hover:bg-gray-800 hover:text-white">
+        {post.title}
+      </Link>
     </div>
   );
 };
--- client/src/redux/features/post/postSlice.js
+++ client/src/redux/features/post/postSlice.js
@@ -34,6 +34,15 @@ export const removePost = createAsyncThunk('post/removePost', async (id) => {
   }
 });
 
+export const updatePost = createAsyncThunk('post/updatePost', async (updatedPost) => {
+  try {
+    const { data } = await axios.put(`/posts/${updatedPost.id}`, updatedPost);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 export const postSlice = createSlice({
   name: 'post',
   initialState,
@@ -71,6 +80,18 @@ export const postSlice = createSlice({
       state.posts = state.posts.filter((post) => post._id !== action.payload._id);
     },
     [removePost.rejected]: (state) => {
+      state.loading = false;
+    },
+    // Edit post
+    [updatePost.pending]: (state) => {
+      state.loading = true;
+    },
+    [updatePost.fulfilled]: (state, action) => {
+      state.loading = false;
+      const index = state.posts.findIndex((post) => post._id === action.payload._id);
+      state.posts[index] = action.payload;
+    },
+    [updatePost.rejected]: (state) => {
       state.loading = false;
     },
   },
