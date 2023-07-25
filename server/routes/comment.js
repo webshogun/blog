5cf1fce42ed6d1a99696a9c786a44094bd33f459
webshogun <webshogun@proton.me>
2023-07-25 21:24:30 +0300

    Creating a comment to a post on backend

---
+++ server/routes/comment.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { checkAuth } from '../utils/checkAuth.js';
+import { createComment } from '../controllers/comments.js';
+const router = new Router();
+
+// Create comment http://localhost:3002/api/comments/:id
+router.post('/:id', checkAuth, createComment);
+
+export default router;
